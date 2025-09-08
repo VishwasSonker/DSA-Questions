de6f050eb38987ec25757c4226a762b3ab879c8d
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-09-08 18:25:09 +0530

    Time: 1 ms (78.19%), Space: 10.9 MB (40.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,7 +8,6 @@ public:
             maxfreq=max(maxfreq, hash[s[r]-'A']);
             if((r-l+1)-maxfreq>k){
                 hash[s[l]-'A']--;
-                maxfreq=0;
                 l++;
             }
             if((r-l+1)-maxfreq<=k){
