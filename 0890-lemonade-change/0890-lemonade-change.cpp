78c7fa3eeeafab1236303aad44a5fd32151a5446
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-09-12 14:41:09 +0530

    Time: 0 ms (100%), Space: 87.2 MB (38.33%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unordered_map<int, int> change = {{5, 0}, {10, 0}};
+        int five=0, ten=0;
         for (int i = 0; i < bills.size(); i++) {
             if (bills[i] == 5) {
-                change[5]++;
+                five++;
             } else if (bills[i] == 10) {
-                if (change[5]>0){
-                    change[5]--;
-                    change[10]++;
+                if (five>0){
+                    five--;
+                    ten++;
                 }else{
                     return false;
                 }
             }else{
-                if(change[5]>0&&change[10]>0){
-                    change[10]--;
-                    change[5]--;
-                }else if(change[5]>=3){
-                    change[5]-=3;
+                if(five>0&&ten>0){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five-=3;
                 }else{
                     return false;
                 }
