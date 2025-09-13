37ce9e7d797436683d423dd2af8d3a8cc63ce735
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-09-13 11:32:57 +0530

    Time: 0 ms (100%), Space: 20.4 MB (63.07%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,15 +3,15 @@ public:
     int jump(vector<int>& nums) {
         int count=0;
         int n= nums.size();
-        int r=n-1;
-        while(r>0){
-            for(int l=0; l<r; l++){
-                if(l+nums[l]>=r){
-                    count++;
-                    r=l;
-                    break;
-                }
+        int l=0, r=0;
+        while(r<n-1){
+            int far = 0;
+            for(int i=l; i<=r; i++){
+                far=max(far, i+nums[i]);
             }
+            l=r+1;
+            r=far;
+            count++;
         }
         return count;
     }
