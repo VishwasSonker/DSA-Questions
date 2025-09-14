9dd83f3c828b95504f4331d204e57c0062549bc9
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-09-14 15:05:27 +0530

    Time: 0 ms (100%), Space: 22.8 MB (62.25%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int> leftpass(n, 1);
-        vector<int> rightpass(n, 1); 
+        vector<int> leftpass(n, 1); 
 
         for(int i=1; i<n; i++){
             if(ratings[i-1]<ratings[i]){
                 leftpass[i]=leftpass[i-1]+1;
             }
-            if(ratings[n-i]<ratings[n-i-1]){
-                rightpass[n-i-1]=rightpass[n-i]+1;
-            }
         }
-        int ans=0;
-        for(int i=0; i<n; i++){
-            ans+=max(leftpass[i], rightpass[i]);
+        int ans=leftpass[n-1];
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i+1]<ratings[i]){
+                leftpass[i]=max(leftpass[i], leftpass[i+1]+1);
+            }
+            ans+=leftpass[i];
         }
         return ans;
     }
