c25691f6a9ece97166351e60141a48514e531324
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-09-24 11:41:57 +0530

    Time: 0 ms (100%), Space: 32 MB (9.2%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -15,15 +15,13 @@ public:
         int ans=0;
         if(!root) return ans;
         queue<TreeNode*> q;
-        vector<vector<int>> arr;
         q.push(root);
         while(!q.empty()){
             int size = q.size();
-            vector<int> temp;
+            ans+=size;
             for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                temp.push_back(node->val);
                 if(node->left){
                     q.push(node->left);
                 }
@@ -31,13 +29,7 @@ public:
                     q.push(node->right);
                 }
             }
-            arr.push_back(temp);
         }
-        int len = arr.size();
-        for(int i=0; i<len-1; i++){
-            ans+=pow(2, i);
-        }
-        ans+=arr[len-1].size();
         return ans;
     }
 };
